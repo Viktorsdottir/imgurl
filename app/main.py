ba593cb1a865ed1eb3beddf8b19b7ab59baad690
Viktorsdottir <vviktorsdottir@gmail.com>
2021-03-23 14:17:03 +0100

    added instruction on how to run the server locally and do queries against it.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile, File, responses
 from pydantic import BaseModel
 from PIL import Image
 import hashlib
+import uvicorn
 import os
 
 app = FastAPI()
@@ -22,6 +23,8 @@ async def predict_api(img_file: UploadFile = File(...)):
     await img_file.read()
     image = Image.open(img_file.file)
     key = hashlib.md5(image.tobytes()).hexdigest()
+    if not os.path.exists("images"):
+        os.makedirs("images")
     if os.path.exists("images/" + key + ".jpg"):
         return {"new": False, "url": "images/" + key}
     else:
@@ -32,3 +35,6 @@ async def predict_api(img_file: UploadFile = File(...)):
 @app.get('/images/{key}')
 async def retrieve_image(key: str):
     return responses.FileResponse("images/" + key + ".jpg")
+
+if __name__ == '__main__':
+    uvicorn.run("app.main:app", host='127.0.0.1', port=5000)
--- readme.md
+++ readme.md
@@ -1,3 +1,31 @@
 **Web server for storing and retrieving images.**
 
+Run the live server:
+
+uvicorn app.main:app --port 5000
+
 Usage example:
+
+Upload an image from path. The server responds with the URL to retrieve the image from:
+
+`curl -X POST -F img_file=@testimages/image1.png http://localhost:5000/images`
+
+{
+"new": true,
+"url": "/images/43d61d"
+}
+
+Storing the same image more than once is accepted and detected, and returns that fact in the "new"
+value, which is false for an image that already was uploaded:
+
+`curl -X POST -F img_file=@testimages/image1.png http://localhost:5000/images`
+
+{
+"new": false,
+"url": "/images/43d61d"
+}
+
+Download an image and save it to foo.jpg
+
+`curl -D - -o foo.jpg http://localhost:5000/images/dfe1481e35c8840bad7d81ad4993d195
+`
